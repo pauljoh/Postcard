3fd53fa7dcf5ef6022275a0bbe3ec95d1ff29edf
PAUL JOHNSON <Paul@PAULs-MacBook-Pro-2.local>
2014-10-26 17:55:10 +0000

    first rev of project

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -9,6 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var enterNameTextField: UITextField!
+    @IBOutlet weak var enterNameMessageField: UITextField!
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +28,23 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func sendMailButtonPressed(sender: UIButton) {
+        messageLabel.hidden = false
+        nameLabel.hidden = false
+        messageLabel.text = enterNameMessageField.text
+        messageLabel.textColor = UIColor.redColor()
+        
+        
+        nameLabel.text = enterNameTextField.text
+        nameLabel.textColor = UIColor.blueColor()
+        
+        
+        enterNameTextField.resignFirstResponder()
+        enterNameTextField.text = ""
+        enterNameMessageField.text = ""
+        enterNameMessageField .resignFirstResponder()
+        
+    }
 
 }
 
